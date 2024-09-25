d11656934bb826c6020b57dd163d9f225f09f861
Kojoel <nanaabruquah1s@gmail.com>
2024-09-25 12:04:23 +0000

    Recommended Component UI added

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { SearchbarComponent } from '../searchbar/searchbar.component';
 import { SidebarComponent } from '../sidebar/sidebar.component';
 import { TrendingComponent } from '../trending/trending.component';
+import { RecommendedComponent } from '../recommended/recommended.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [SearchbarComponent, SidebarComponent, TrendingComponent],
+  imports: [SearchbarComponent, SidebarComponent, TrendingComponent, RecommendedComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
